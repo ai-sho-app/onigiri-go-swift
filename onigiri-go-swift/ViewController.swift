5f959e123d67ffe7ff319d0dbe034f7f32d8d54b
shkwsk <ren7link@gmail.com>
2019-07-20 16:07:46 +0900

    Initial Commit

---
+++ onigiri-go-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  onigiri-go-swift
+//
+//  Created by Sho Kawasaki on 2019/07/20.
+//  Copyright © 2019 Sho Kawasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
