2d0475d1d85c1e500991beee67b92cb9bcc7ad0b
shkwsk <ren7link@gmail.com>
2019-09-26 02:09:15 +0900

    add: progress stop workout button

--- onigiri-go-swift/App/Workout/WorkoutViewController.swift
+++ onigiri-go-swift/App/Workout/WorkoutViewController.swift
@@ -7,25 +7,74 @@
 //
 
 import Foundation
+import UICircularProgressRing
 
 import UIKit
 
 class WorkoutViewController: UIViewController {
 
-    let backButton = UIButton(type: .custom)
+    lazy var progressRing: UICircularProgressRing = {
+        var ring = UICircularProgressRing()
+        ring.frame = CGRect(x: self.view.center.x - 50, y:self.view.center.y - 50, width:100, height:100)
+        // ring.center = self.view.center
+        ring.maxValue = 100
+        ring.style = .ontop
+        ring.gradientOptions = UICircularRingGradientOptions(startPosition: .topRight,
+                                                             endPosition: .topLeft,
+                                                             colors: [UIColor.themeOrange],
+                                                             colorLocations: [1.0])
+        return ring
+    }()
+
+    lazy var stopWorkoutButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "btn_stop"), for: .normal)
+        button.frame = CGRect(x:0, y:0, width:100, height:100)
+        button.center = self.view.center
+        button.setTitle("back!", for: .normal)
+        let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(pressStopWorkout(sender:)))
+        longGesture.minimumPressDuration = 0.5
+        button.addGestureRecognizer(longGesture)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // TODO: view design
-        self.view.backgroundColor = UIColor.blue
+        self.view.backgroundColor = UIColor.white
+
+        view.addSubview(progressRing)
+        // TODO: button design
+        view.addSubview(stopWorkoutButton)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
-    @objc private func stopWorkout(sender: UIButton) {
-        print("Press stop workout button from \(sender)")
+    @objc private func pressStopWorkout(sender: UILongPressGestureRecognizer) {
+        // print("Press stop workout button from \(sender)")
+        switch sender.state {
+        case .began:
+            self.progressRing.startProgress(to: 100, duration: 2.0) {
+                print("Done animating!")
+                // home画面に戻る
+                self.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
+            }
+        case .ended:
+            self.progressRing.resetProgress()
+        case .possible:
+            break
+        case .changed:
+            break
+        case .cancelled:
+            break
+        case .failed:
+            break
+        @unknown default:
+            break
+        }
     }
 }
